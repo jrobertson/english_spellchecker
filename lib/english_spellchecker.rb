49926d13abca9fd2e9b4ad6b6bed08eef45ae7e4
James Robertson <digital.robertson@gmail.com>
2022-10-30 19:32:07 +0000

    improvement: Words which match at the beginning but with a different ending are now split into an Array object.

--- lib/english_spellchecker.rb
+++ lib/english_spellchecker.rb
@@ -8,25 +8,86 @@ require 'did_you_mean'
 
 
 class EnglishSpellcheck
+  
+  @words = WordsDotDat.words
 
   def initialize()
-    @spelling = DidYouMean::SpellChecker.new(dictionary: WordsDotDat.words)
+    @words = WordsDotDat.words
+    @spelling = DidYouMean::SpellChecker.new(dictionary: @words)
   end
 
-  def self.spell(w)
-
-    r = DidYouMean::SpellChecker.new(dictionary: WordsDotDat.words).correct(w)
-    r.any? ? r.first : nil
+  def self.spell(raww, verbose: true)
+    
+    w = raww.downcase
+    
+    return raww  if @words.include? w
+    r = DidYouMean::SpellChecker.new(dictionary: @words).correct(w)
+    
+    if r.any? then
+      
+      found = r.first
+      puts 'found: ' + found.inspect if verbose
+      
+      # return the word found along with the ending of the keyword which 
+      # wasn't found.
+      #
+      if raww.length > found.length then
+        if found == w[0..found.length-1] then
+          return [found, raww[found.length..-1]]
+        else
+          return found
+        end
+      else
+        if w == found[0..raww.length-1] then
+          return [raww, found[raww.length..-1]]
+        else
+          return found
+        end
+      end
+      
+    else
+      nil
+    end
 
   end
 
   def inspect()
-    "<EnglishSpellcheck::#{self.object_id} @spelling=DidYouMean::SpellChecker>"
+    "#<EnglishSpellcheck:#{self.object_id} @spelling=DidYouMean::SpellChecker>"
   end
 
-  def spell(w)
+  def spell(raww, verbose: true)
+    
+    w = raww.downcase
+    
+    return raww  if @words.include? w
     r = @spelling.correct w
-    r.any? ? r.first : nil
+    
+    if r.any? then
+      
+      found = r.first
+      puts 'found: ' + found.inspect if verbose
+
+      # return the word found along with the ending of the keyword which 
+      # wasn't found.
+      #
+      if raww.length > found.length then
+        if found == w[0..found.length-1] then
+          return [found, raww[found.length..-1]]
+        else
+          return found
+        end
+      else
+        if w == found[0..raww.length-1] then
+          return [raww, found[raww.length..-1]]
+        else
+          return found
+        end
+      end
+      
+    else
+      nil
+    end
+    
   end
 
   alias spelling spell
